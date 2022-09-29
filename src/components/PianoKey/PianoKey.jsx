609d6f1fcc211eca3f7a6e6da5327f2e952aa455
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-09-29 14:35:43 +0100

    used keyboard object and event listener to map keystrokes to piano keys

--- src/components/PianoKey/PianoKey.jsx
+++ src/components/PianoKey/PianoKey.jsx
@@ -2,27 +2,31 @@ import React from 'react'
 import { useState } from 'react';
 import "./PianoKey.scss"
 import useSound from 'use-sound';
-import useKeypress from 'react-use-keypress';
 
 const PianoKey = (props) => {
 
     const {note, keycode, classnames} = props;
     const [playNote]  = useSound(note);
-    const [style, setStyle] = useState("")
+    const [style, setStyle] = useState("");
+    
 
     const addStyling = () => setStyle("click");
     const removeStyling = () => setStyle("");
 
-      useKeypress([keycode], (event) => {
-        console.log("hello");
-        if (event.key == keycode) {
-          console.log("got here");
-          playNote();
-        }
-    })
+    const handleClick = () => {
+      playNote();
+    }
+
+    const handleKeyDown = (e) => {
+      if (e.keyCode === keycode) {
+        playNote();
+      }
+    };
+  
+    document.addEventListener("keydown", handleKeyDown, false);
 
   return (
-    <div className={`${classnames} ${style}`} onClick={playNote} onMouseDown={addStyling} onMouseUp={removeStyling} onKeyDown={useKeypress}>
+    <div className={`${classnames} ${style}`} onClick={handleClick} onMouseDown={addStyling} onMouseUp={removeStyling} onKeyDown={handleKeyDown}>
     </div>
   )
 }
--- src/containers/Piano/Piano.jsx
+++ src/containers/Piano/Piano.jsx
@@ -16,25 +16,26 @@ import C4 from "../../audio/C4.mp3";
 
 
 import "./Piano.scss"
+import KEYBOARD from '../../Keyboard';
 
 const Piano = () => {
 
 
   return (
     <div className='piano'>
-    <PianoKey classnames={`piano__key c3 white`} note={C3} keycode={"122"}/>
-    <PianoKey classnames={`piano__key db3 black`} note={Db3} keycode={"115"}/>
-    <PianoKey classnames={`piano__key d3 white`} note={D3} keycode={"120"}/>
-    <PianoKey classnames={`piano__key eb3 black`} note={Eb3} keycode={"100"} />
-    <PianoKey classnames={'piano__key e3 white'} note={E3} keycode={"99"}/>
-    <PianoKey classnames={'piano__key f3 white'} note={F3} keycode={"118"}/>
-    <PianoKey classnames={'piano__key gb3 black'}note={Gb3} keycode={"103"}/>
-    <PianoKey classnames={'piano__key g3 white'} note={G3} keycode={"98"} />
-    <PianoKey classnames={'piano__key ab3 black'} note={Ab3} keycode={"104"} />
-    <PianoKey classnames={'piano__key a3 white'} note={A3} keycode={"110"} />
-    <PianoKey classnames={'piano__key bb3 black'} note={Bb3} keycode={"106"}/>
-    <PianoKey classnames={'piano__key b3 white'} note={B3} keycode={"109"}/>
-    <PianoKey classnames={'piano__key c4 white'} note={C4} keycode={"44"}/>
+    <PianoKey classnames={`piano__key c3 white`} note={C3} keycode={KEYBOARD.KEY_Z}/>
+    <PianoKey classnames={`piano__key db3 black`} note={Db3} keycode={KEYBOARD.KEY_S}/>
+    <PianoKey classnames={`piano__key d3 white`} note={D3} keycode={KEYBOARD.KEY_X}/>
+    <PianoKey classnames={`piano__key eb3 black`} note={Eb3} keycode={KEYBOARD.KEY_D} />
+    <PianoKey classnames={'piano__key e3 white'} note={E3} keycode={KEYBOARD.KEY_C}/>
+    <PianoKey classnames={'piano__key f3 white'} note={F3} keycode={KEYBOARD.KEY_V}/>
+    <PianoKey classnames={'piano__key gb3 black'}note={Gb3} keycode={KEYBOARD.KEY_G}/>
+    <PianoKey classnames={'piano__key g3 white'} note={G3} keycode={KEYBOARD.KEY_B} />
+    <PianoKey classnames={'piano__key ab3 black'} note={Ab3} keycode={KEYBOARD.KEY_H} />
+    <PianoKey classnames={'piano__key a3 white'} note={A3} keycode={KEYBOARD.KEY_N} />
+    <PianoKey classnames={'piano__key bb3 black'} note={Bb3} keycode={KEYBOARD.KEY_J}/>
+    <PianoKey classnames={'piano__key b3 white'} note={B3} keycode={KEYBOARD.KEY_M}/>
+    <PianoKey classnames={'piano__key c4 white'} note={C4} keycode={KEYBOARD.COMMA}/>
     </div>
   )
 }
