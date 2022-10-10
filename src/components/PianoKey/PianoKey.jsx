61873c752ba8316f79b8605417979c287a8570d4
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-10-10 20:16:02 +0100

    working on input toggle

--- src/components/PianoKey/PianoKey.jsx
+++ src/components/PianoKey/PianoKey.jsx
@@ -5,35 +5,45 @@ import useSound from 'use-sound';
 
 const PianoKey = (props) => {
 
-    const {note, keycode, classnames, isKeyboard} = props;
-    const [playNote]  = useSound(note);
-    const [clickStyle, setClickStyle] = useState("");
-    
+  const { note, keycode, classnames, isKeyboard } = props;
+  const [playNote] = useSound(note);
+  const [clickStyle, setClickStyle] = useState("");
 
-    const addStyling = () => setClickStyle("click");
-    const removeStyling = () => setClickStyle("");
 
-    const handleClick = () => {
+  const addStyling = () => setClickStyle("click");
+  const removeStyling = () => setClickStyle("");
+
+  const handleClick = () => {
+    playNote();
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === keycode) {
       playNote();
     }
+  };
 
-    const handleKeyDown = (e) => {
-      if (e.keyCode === keycode) {
-        playNote();
-      }
-    };
-  
-    document.addEventListener("keydown", handleKeyDown, false);
-
-    const checkIsKeyboard = (currentIsKeyboard, click) => {
-        if (!currentIsKeyboard) {
-          click();
-        }
+  const checkIsKeyboard = (currentIsKeyboard, click) => {
+    if (!currentIsKeyboard) {
+      click();
     }
-    // onMouseDown={!isKeyboard ? addStyling : undefined} onMouseUp={!isKeyboard ? removeStyling : undefined} onKeyDown={isKeyboard ? handleKeyDown : undefined}
+  }
+
+    // document.addEventListener("keydown", handleKeyDown, false);
+
+    // const handleEventListener = (e) => {
+    // document.addEventListener("keydown", (e) => {
+    //   checkIsKeyboard(isKeyboard, handleKeyDown(e));
+    // })}
+
+
+  
+
+
+  // onMouseDown={!isKeyboard ? addStyling : undefined} onMouseUp={!isKeyboard ? removeStyling : undefined} onKeyDown={() => checkIsKeyboard(isKeyboard, handleKeyDown)}
 
   return (
-    <div className={`${classnames} ${clickStyle}`} onClick={() => checkIsKeyboard(isKeyboard, handleClick)} >
+    <div className={`${classnames} ${clickStyle}`} onClick={() => checkIsKeyboard(isKeyboard, handleClick)} onKeyDown={() => checkIsKeyboard(isKeyboard, () => document.addEventListener("keydown", handleKeyDown, false))} >
     </div>
   )
 }
